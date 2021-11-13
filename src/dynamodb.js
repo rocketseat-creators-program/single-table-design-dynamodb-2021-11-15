d39ef39dd43d8752e6afdb5eda0ef18282feeac8
André Kanayama <andre.spanguero@gmail.com>
2021-11-13 13:29:26 -0300

    Update dynamodb.js

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -4,25 +4,77 @@ AWS.config.loadFromPath('./config.json')
 
 const documentClient = new AWS.DynamoDB.DocumentClient()
 
-const createUser = () => {
+const tableName = 'ticket-application'
+
+const createUser = (username, name, email, address) => {
+  const params = {
+    TableName: tableName,
+    Item: {
+      pk: `USER#${username}`,
+      sk: `PROFILE#${username}`,
+      name,
+      email,
+      address,
+    },
+  }
+
+  return documentClient.put(params).promise(console.log)
 }
 
-const createTicket = () => {
+const createTicket = (username, title, body, status) => {
+  const params = {
+    TableName: tableName,
+    Item: {
+      pk: `USER#${username}`,
+      sk: `TICKET#${Math.floor(new Date().getTime() / 1000)}`,
+      title,
+      body,
+      status,
+    },
+  }
+
+  return documentClient.put(params).promise(console.log)
 }
 
-const getUser = () => {
+const getUser = username => {
+  const params = {
+    TableName: tableName,
+    Key: { pk: `USER#${username}`, sk: `PROFILE#${username}` },
+  }
+
+  return documentClient.get(params).promise().then(console.log)
 }
 
-const getUserWithTickets = () => {
+const getUserTickets = username => {
+  const params = {
+    TableName: tableName,
+    KeyConditionExpression: 'pk = :pk and begins_with(sk, :sk)',
+    ExpressionAttributeValues: {
+      ':pk': `USER#${username}`,
+      ':sk': 'TICKET#',
+    },
+  }
+
+  return documentClient.query(params).promise().then(console.log)
 }
 
-const getTickets = () => {
+const getUserAndTickets = username => {
+  const params = {
+    TableName: tableName,
+    KeyConditionExpression: 'pk = :pk',
+    ScanIndexForward: true,
+    ExpressionAttributeValues: {
+      ':pk': `USER#${username}`,
+    },
+  }
+
+  return documentClient.query(params).promise().then(console.log)
 }
 
 module.exports = {
   createUser,
   createTicket,
   getUser,
-  getUserWithTickets,
-  getTickets,
+  getUserTickets,
+  getUserAndTickets,
 }
